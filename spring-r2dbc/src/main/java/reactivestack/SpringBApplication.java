9245e31e6071dc0b3807082f60dd392b9e787d25
Zoltán Domahidi <domahidizoltan@gmail.com>
2018-11-02 15:23:39 +0100

    Create project skeleton for spring-r2dbc

---
+++ spring-r2dbc/src/main/java/reactivestack/SpringBApplication.java
@@ -0,0 +1,13 @@
+package reactivestack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBApplication.class, args);
+    }
+
+}
