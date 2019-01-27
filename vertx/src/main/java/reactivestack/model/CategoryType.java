646301472c3589800f882299d5091d4fbf7df96f
Zoltán Domahidi <domahidizoltan@gmail.com>
2019-01-27 09:04:49 +0100

    Update category type

--- vertx/src/main/java/reactivestack/model/CategoryType.java
+++ vertx/src/main/java/reactivestack/model/CategoryType.java
@@ -8,7 +8,7 @@ public enum CategoryType {
     SMILEYS_AND_EMOTION("Smileys & Emotion"),
     PEOPLE_AND_BODY("People & Body"),
     COMPONENT("Component"),
-    NATURE("Nature"),
+    ANIMALS_AND_NATURE("Animals & Nature"),
     FOOD_AND_DRINK("Food & Drink"),
     TRAVEL_AND_PLACES("Travel & Places"),
     ACTIVITIES("Activities"),
